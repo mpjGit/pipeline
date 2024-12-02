b21a1ce99668218cbe8f25aaebd913205235c104
zhoupf <422081094@qq.com>
2024-12-02 09:33:03 +0800

    feta: 默认路由

--- src/router/index.js
+++ src/router/index.js
@@ -60,7 +60,7 @@ const routes = [
                 path: '',
                 redirect: () => {
                     return {
-                        path: 'monitor'
+                        path: 'deviceall'
                     }
                 }
             },
